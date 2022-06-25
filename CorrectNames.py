63128c7b5075e00c5cb56dee58d0471874a57d81
Mostafa Mohammadzadeh <mostafa.mdzh@gmail.com>
2022-06-25 15:34:37 +0430

    CorrectNames: main file added

---
+++ CorrectNames.py
@@ -0,0 +1,47 @@
+# mostafa.mdzh@gmail.com | @MostafaMDZH
+
+import json
+from pathlib import Path
+import os
+
+# open the list:
+listFile = open('list.json')
+list = json.load(listFile)
+num = 1
+
+for i in list['list']:
+    
+    # name & dir:
+    oldName = i["oldName"]
+    dir     = i["path"   ]
+    newName = i["newName"]
+    
+    # path
+    oldPath = Path("Gallery" + dir + "/" + oldName)
+    newPath = Path("Gallery" + dir + "/" + newName)
+    
+    # loop to files:
+    for j in range(1, 100):
+        
+        # main file:
+        oldFullName = str(oldPath) + "." + str(j) + ".jpg"
+        file = Path(oldFullName)
+        if file.is_file():
+            newFullName = str(newPath) + "." + str(j) + ".jpg"
+            os.rename(oldFullName, newFullName)
+            print('renamed[' + str(num) + ']: ' + oldFullName + '     to: ' + newFullName)
+        
+        # .min file
+        oldFullName = str(oldPath) + "." + str(j) + ".min.jpg"
+        file = Path(oldFullName)
+        if file.is_file():
+            newFullName = str(newPath) + "." + str(j) + ".min.jpg"
+            os.rename(oldFullName, newFullName)
+            print('renamed[' + str(num) + ']: ' + oldFullName + ' to: ' + newFullName)
+
+        else:
+            if j != 1: num += 1
+            break
+    
+    if j == 1:
+        print('not found: ' + str(oldPath))
